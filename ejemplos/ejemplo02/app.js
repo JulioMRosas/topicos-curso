dbee956d8d33ff2a47b3c8e5a266be2b45464221
ibby <al21760482@ite.edu.mx>
2023-08-30 11:56:52 -0700

    quitando las alertas del ejemplo02

--- ejemplos/ejemplo02/app.js
+++ ejemplos/ejemplo02/app.js
@@ -7,17 +7,17 @@ function handleEvent(e) {
 
     if (!username) {
         // console.log("Introduce tu usuario");
- /*        const alerta = document.createElement('p');
+        const alerta = document.createElement('p');
         alerta.innerHTML = "¡Introduce tu usuario!";
-        document.body.appendChild(alerta); */
-        alert("¡Introduce tu usuario!")
+        document.body.appendChild(alerta);
+        // alert("¡Introduce tu usuario!")
     } 
     else if (password.length <= 5) {
         // console.log("Tu contraseña debe tener más de 5 caracteres");
-    /*      const alerta = document.createElement('p');
-            alerta.innerHTML = "¡Tu contraseña debe tener más de 5 caracteres!";
-            document.body.appendChild(alerta); */
-        alert("¡Tu contraseña debe tener más de 5 caracteres!")
+        const alerta = document.createElement('p');
+        alerta.innerHTML = "¡Tu contraseña debe tener más de 5 caracteres!";
+        document.body.appendChild(alerta);
+        // alert("¡Tu contraseña debe tener más de 5 caracteres!")
     }
     else {
         // console.log("Bienvenido");
---
+++ ejemplos/ejemplo3/app.js
@@ -0,0 +1,10 @@
+// Arrays, Vectores
+
+const estudiantes = ['Daniela', 'Liz', 'Adolfo', 'Chris'];
+
+for (let i = 0; i < estudiantes.length; i++) {
+    console.log(estudiantes[i]);
+}
+
+// Funciones
+
